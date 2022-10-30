1fb65d6d25778ed176f877909250b153d2a4fca9
dog23121 <killian.osborne@gmail.com>
2022-10-30 02:27:04 -0700

    Movement

--- Space Game/Assets/Scripts/Movment/PlayerMovment.cs
+++ Space Game/Assets/Scripts/Movment/PlayerMovment.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController controler;
 
-    // Update is called once per frame
+    public float speed = 12f;
+    public float gravity = -20f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velosity;  
+    bool isGrounded;
+
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velosity.y < 0)
+        {
+            velosity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controler.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velosity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
+        velosity.y += gravity * Time.deltaTime;
+
+        controler.Move(velosity * Time.deltaTime);
     }
 }
